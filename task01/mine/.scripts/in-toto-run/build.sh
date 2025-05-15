5202b00ac581701dfb518314a62b1afbdb52c399
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2025-05-15 19:56:27 -0400

    feat(task01): adding in-toto-run attestation

--- task01/mine/.scripts/in-toto-run/build.sh
+++ task01/mine/.scripts/in-toto-run/build.sh
@@ -9,4 +9,4 @@ echo $0
     # p: products (outputs)
     # m: materials (inputs)
     # n: step name
-op run --env-file=".env" -- in-toto-run --use-dsse --gpg GPGKEYID -P GPGPASS -n compile -m $1.c policy.rego Makefile -p $1.ko $1.mo* $1.o modules.order Module.symvers ./*.cmd -d sigs -- make build
+op run --env-file=".env" -- in-toto-run --use-dsse --gpg "GPGKEYID" -P "GPGPASS" -n compile -m $1.c policy.rego Makefile -p $1.ko $1.mo* $1.o modules.order Module.symvers ./*.cmd -d sigs -- make build
