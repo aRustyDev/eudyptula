ce8b3c6f58c4326cca53e545f85c4dac46519c83
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2025-05-16 01:07:35 -0400

    feat(task01): switching to witness based attestation

--- .scripts/apt-get.sh
+++ .scripts/apt-get.sh
@@ -11,3 +11,6 @@ sudo apt-get upgrade -y
 
 echo ":: apt-get install"
 sudo apt install -y golang-go python3-pip make kmod dwarves gcc linux-headers-$(KERNEL_VERSION) libappindicator1 gnupg2
+
+echo ":: install witness"
+bash <(curl -s https://raw.githubusercontent.com/in-toto/witness/main/install-witness.sh)
--- task01/mine/.scripts/in-toto-run/build.sh
+++ task01/mine/.scripts/in-toto-run/build.sh
@@ -9,4 +9,14 @@ echo $0
     # p: products (outputs)
     # m: materials (inputs)
     # n: step name
-op run --env-file=".env" -- in-toto-run --use-dsse --gpg -n compile -m $1.c policy.rego Makefile -p $1.ko $1.mo* $1.o modules.order Module.symvers ./*.cmd -d sigs -- make build
+witness run -s compile -o attest.json \
+    --attestor-product-exclude-glob "$1.c" \
+    --attestor-product-include-glob "*$1*" \
+    --attestor-product-include-glob "*odule*" \
+	--signer-fulcio-url https://fulcio.sigstore.dev \
+	--signer-fulcio-oidc-client-id sigstore \
+	--signer-fulcio-oidc-issuer https://oauth2.sigstore.dev/auth \
+	--timestamp-servers https://freetsa.org/tsr -- make build
+
+# -m $1.c policy.rego Makefile
+# -p $1.ko $1.mo* $1.o modules.order Module.symvers ./*.cmd
